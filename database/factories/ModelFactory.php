94069c77d4f96a3a1cc03deb7ef57bdf059e388a
claudio <viadigital@debianLenovo.casa>
2018-03-09 15:58:53 -0300

    factory incompleto

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+       'name' => 'admin',
+       'email' => 'vicomser.claudio@gmail.com',
+        //'name' => $faker->name,
+        //'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'type'=>'admin',
+        'country'=>'arg',
+        'status'=>'activo'
     ];
+});
+
+$factory->define(App\Category::class,function(Faker\Generator $faker){
+  return [
+    'description' => $faker->username(),
+  ];
+});
+
+
+$factory->define(App\New::class,function(Faker\Generator $faker){
+  return [
+     'categoryId'=> $faker->numberBetween($min=10,$max=5),
+     'userId'=> '1',
+     'title'=> str_radom(20),
+     'content'=> str_radom(50),
+     'statusN'=> str_radom(50),
+  ];
 });
