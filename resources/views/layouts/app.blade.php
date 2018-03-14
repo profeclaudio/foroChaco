65dd5525b62a39dd7151e96ce1ca0d164e439575
claudio sosa <profeclaudiososa@yahoo.com.ar>
2018-03-14 19:07:07 -0300

    avance en index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Foro Chaco Sudamericado</title>
+
+        <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}">
         <!-- Plugins CSS -->
         <link href="{{asset('css/plugins/plugins.css')}}" rel="stylesheet">
         <!-- REVOLUTION STYLE SHEETS -->
