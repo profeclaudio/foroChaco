13b50eb512800a60d33693140bc4dfc39cf0f25e
Claudio Sosa <vicomser.claudio@gmail.com>
2019-01-19 11:08:21 -0300

    listado de noticias

--- resources/views/news/list.blade.php
+++ resources/views/news/list.blade.php
@@ -48,15 +48,12 @@
                     
                 @endforelse
             
+            {{-- <div class="mt-2 mx-auto shop_pagination_area">
+                {{$products->links('pagination::bootstrap-4')}}
+            </div>     --}}
           
             <nav aria-label="Page navigation example" class="mb70">
-                <ul class="pagination pagination justify-content-end">
-                    <li class="page-item "><a class="page-link" href="#">Previous</a></li>
-                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                </ul>
+                    {{$publications->links('pagination::bootstrap-4')}}
             </nav>
         </div>
     </div>
